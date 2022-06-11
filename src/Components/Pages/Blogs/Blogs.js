8249dac62fc050c73ecf9ee0be16fd4c880f5470
rashedsabbir <rashedulhasan1996@gmail.com>
2022-06-11 18:34:53 +0600

    blogs added

--- src/Components/Pages/Blogs/Blogs.js
+++ src/Components/Pages/Blogs/Blogs.js
@@ -4,10 +4,12 @@ import banner from '../../../images/banner/banner.jpg'
 
 const Blogs = () => {
     return (
-        <div class="hero" >
+        <div>
+            <h1 className='text-teal-500 text-4xl font-bold p-10'>Blogs Coming Soon...</h1>
+            <div class="hero" >
             <img src={banner} class="max-w-full h-auto rounded-lg mx-6" />
-  
-    
+</div>
+
 </div>
     );
 };
